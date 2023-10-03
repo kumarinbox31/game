a3a43e0b057678ccc92de53e1e94ea2f98153121
kumarinbox31 <kumarinbox31@gmail.com>
2023-10-03 08:38:34 +0530

    initial

--- footer.php
+++ footer.php
@@ -1,3 +1,116 @@
+<!-- Footer Start -->
+<div class="main-footer w-100">
+    <div class="top-row"></div>        
+    <div class="bottom-row d-flex justify-content-between align-items-center">
+
+        <!-- Menu Item Start -->
+        <div class="footer-menu-item text-center item-overview">
+            <a href="home.html" class="no-text-decoration">
+                <img src="assets/img/icons/overviews.svg" class="pasive" alt="Overview Icon">
+                <img src="assets/img/icons/overviews-active.svg" class="active" alt="Overview Active Icon">
+                <div class="menu-title">
+                    Overview
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item Start -->
+            <a href="home.html" class="no-text-decoration dark-theme-menu-item">
+                <img src="assets/img/icons/overviews-dark-theme.svg" class="pasive" alt="Overview Icon">
+                <img src="assets/img/icons/overviews-active-dark-theme.svg" class="active" alt="Overview Active Icon">
+                <div class="menu-title">
+                    Overview
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item End -->
+        </div>
+        <!-- Menu Item End -->
+
+        <!-- Menu Item Start -->
+        <div class="footer-menu-item text-center item-pages">
+            <a href="all-pages.html" class="no-text-decoration">
+                <img src="assets/img/icons/pages.svg" class="pasive" alt="Pages Icon">
+                <img src="assets/img/icons/pages-active.svg" class="active" alt="Pages Active Icon">
+                <div class="menu-title">
+                    Pages
+                    <div class="notification-number">2</div>
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item Start -->
+            <a href="all-pages.html" class="no-text-decoration dark-theme-menu-item">
+                <img src="assets/img/icons/pages-dark-theme.svg" class="pasive" alt="Pages Icon">
+                <img src="assets/img/icons/pages-active-dark-theme.svg" class="active" alt="Pages Active Icon">
+                <div class="menu-title">
+                    Pages
+                    <div class="notification-number">2</div>
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item End -->
+        </div>
+        <!-- Menu Item End -->
+
+        <!-- Menu Item Start -->
+        <div class="footer-menu-item text-center item-components">
+            <a href="components.html" class="no-text-decoration">
+                <img src="assets/img/icons/components.svg" class="pasive" alt="Components Icon">
+                <img src="assets/img/icons/components-active.svg" class="active" alt="Components Icon">
+                <div class="menu-title">
+                    Components
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item Start -->
+            <a href="components.html" class="no-text-decoration dark-theme-menu-item">
+                <img src="assets/img/icons/components-dark-theme.svg" class="pasive" alt="Components Icon">
+                <img src="assets/img/icons/components-active-dark-theme.svg" class="active" alt="Components Icon">
+                <div class="menu-title">
+                    Components
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item End -->
+        </div>
+        <!-- Menu Item End -->
+
+        <!-- Menu Item Start -->
+        <div class="footer-menu-item text-center item-cards">
+            <a href="my-cards.html" class="no-text-decoration">
+                <img src="assets/img/icons/card.svg" class="pasive" alt="My Cards Icon">
+                <div class="menu-title">
+                    My Cards
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item Start -->
+            <a href="my-cards.html" class="no-text-decoration dark-theme-menu-item">
+                <img src="assets/img/icons/card-dark-theme.svg" class="pasive" alt="My Cards Icon">
+                <div class="menu-title">
+                    My Cards
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item End -->
+        </div>
+        <!-- Menu Item End -->
+
+        <!-- Menu Item Start -->
+        <div class="footer-menu-item text-center item-settings">
+            <a href="settings.html" class="no-text-decoration">
+                <img src="assets/img/icons/settings.svg" alt="Settings Icon">
+                <div class="menu-title">
+                    Settings
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item Start -->
+            <a href="settings.html" class="no-text-decoration dark-theme-menu-item">
+                <img src="assets/img/icons/settings-dark-theme.svg" class="pasive" alt="Settings Icon">
+                <img src="assets/img/icons/settings-active-dark-theme.svg" class="active" alt="Settings Active Icon">
+                <div class="menu-title">
+                    Settings
+                </div>
+            </a>
+            <!-- Dark Theme Menu Item End -->
+        </div>
+        <!-- Menu Item End -->
+
+    </div>
+</div>
+<!-- Footer End -->
+
 <script type="text/javascript" src="assets/js/main.min.js"></script>
 </body>
 </html>
